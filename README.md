2d846e163254d99ce219ab1a55109c85b4b2850f
Einari Happonen <allu.happonen@gmail.com>
2022-02-24 13:50:42 +0200

    debug mode added

--- README.md
+++ README.md
@@ -13,9 +13,10 @@ You will need node & npm installed in your environment.
 
 ## Configuration (.env file)
 
-    SERVER_PORT - The port for the express server
-    BASE_URL - The base url for the web UI (used for script and css paths)
-    PRECISION - The max precision in digits (amount of decimals) for result numbers
+    SERVER_PORT - The port number for the express server
+    BASE_URL - The base url string for the web UI (used for script and css paths)
+    PRECISION - The max precision number in digits (amount of decimals) for result numbers
+    DEBUG - More verbose console output boolean (true | false)
 
 ## Running tests
 
@@ -27,7 +28,7 @@ You will need node & npm installed in your environment.
 
 ## Web UI for development
 
-    Browse to http://localhost:3030 (the number is the SERVER_PORT in the .env file)
+    Browse to http://localhost:3040 (the number is the SERVER_PORT in the .env file)
 
 ## Running in production (manual build & start)
 
--- src/modules/server.ts
+++ src/modules/server.ts
@@ -8,21 +8,25 @@ import Validator from './validator'
 export default class Server {
   static instance?: Server
   private server?: http.Server
+  private debug = false
 
   constructor() {
     if (Server.instance) {
       return Server.instance
     }
     Server.instance = this
+    this.debug = process.env.DEBUG === 'true'
   }
 
   start(): http.Server {
     const date = new Date()
     console.log(`${date.toJSON()} - Starting AutoMUKit API server...`)
+    this.debugLog('Debug mode is on')
     const app = express()
     app.use(express.json())
     app.set('view engine', 'pug')
     app.get('/', function (req, res) {
+      console.log(`${date.toJSON()} - Processing GET request from ${req.headers.referer}`)
       res.render('index', { baseUrl: process.env.BASE_URL })
     })
     app.use(express.static('public'))
@@ -39,29 +43,47 @@ export default class Server {
   }
 
   private handlePost(req: express.Request, res: express.Response): void {
+    const date = new Date()
+    console.log(`${date.toJSON()} - Processing POST request from ${req.headers.referer}`)
     const contentType = req.get('Content-type')
+    this.debugLog(`Handling POST request body: ${JSON.stringify(req.body, null, 2)}`)
     if (!contentType || contentType.indexOf('application/json') === -1) {
+      this.debugLog(`Invalid content type: ${contentType}. Responding with 415.`)
       res.sendStatus(415)
       return
     }
     const errors = Validator.validateInputFormat(req.body)
     if (errors.length > 0) {
+      this.debugLog(`Input format validation failed: ${JSON.stringify(errors)}. Responding with 400.`)
       res.status(400).send({ errors })
-    } else {
-      const results = []
-      try {
-        const input = req.body as Input
-        const precision = process.env.PRECISION !== undefined ? parseInt(process.env.PRECISION, 10) : 0
-        const mukit = new MUKit(input, process.env.NODE_ENV === 'test' ? 2 : precision)
-        for (const range of input.ranges) {
-          results.push(mukit.calculateResult(range))
-        }
-      } catch (e) {
-        const msg = e instanceof Error ? e.message : String(e)
-        res.status(422).send({ errors: [msg] })
-        return
-      }
-      res.send(results)
+      return
     }
+    const results = []
+    try {
+      const input = req.body as Input
+      const precision = process.env.PRECISION !== undefined ? parseInt(process.env.PRECISION, 10) : 0
+      const mukit = new MUKit(input, process.env.NODE_ENV === 'test' ? 2 : precision)
+      for (const range of input.ranges) {
+        this.debugLog(`Calculating result for range: ${JSON.stringify(range)}.`)
+        results.push(mukit.calculateResult(range))
+      }
+    } catch (e) {
+      const msg = e instanceof Error ? e.message : String(e)
+      this.debugLog(`Error thrown: ${msg}. Responding with 422.`)
+      if (e instanceof  Error) {
+        this.debugLog(`Stack trace: ${e.stack}`)
+      }
+      res.status(422).send({ errors: [msg] })
+      return
+    }
+    this.debugLog(`Response results: ${JSON.stringify(results, null, 2)}.`)
+    res.send(results)
+  }
+
+  debugLog(msg: string) {
+    if (!this.debug) {
+      return
+    }
+    console.debug(msg)
   }
 }
